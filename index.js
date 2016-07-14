b2691a84933651ab9203b8b4f8dc8e0b8fdeba60
Michael Williams <michael.williams@enspiral.com>
2016-07-14 22:23:42 +1200

    it's happening!

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const extend = require('xtend')
+const mapValues = require('@f/map-values')
+const mapObj = require('@f/map-obj')
 const pullMany = require('pull-many')
-
-// TODO publish somewhere else
-// undefined === "we're not sure if it's anything"
-// null === "it's nothing"
+const empty = require('pull-stream/sources/empty')
+const multi = require('inu-multi')
 
 module.exports = {
   createAction,
+  combineApps,
+  combineInits,
+  combineUpdates,
+  scopeUpdate,
   handleActions,
   reduceUpdates,
   runMany
@@ -18,6 +22,47 @@ function createAction (type, payloadCreator = identity) {
     payload: payloadCreator(...args)
   })
 }
+
+function combineApps (apps) {
+  return multi({
+    init: combineInits(mapObj(app => app.init, apps)),
+    update: combineUpdates(mapObj(app => app.update, apps)),
+    run: runMany(mapValues(app => app.run, apps))
+    // TODO combine routes?
+  })
+}
+
+function combineInits (inits) {
+  return function combinedInit () {
+    var effect = []
+    const model = mapObj(init => {
+      const state = init()
+      if (state.effect) effect.push(state.effect)
+      return state.model
+    }, inits)
+    return { model, effect }
+  }
+}
+
+function combineUpdates (updates) {
+  return reduceUpdates(
+    mapValues(scopeUpdate, updates)
+  )
+}
+
+function scopeUpdate (update, key) {
+  return function scopedUpdate (model, action) {
+    const scopeState = update(model[key], action)
+    const nextModel = extend(model, {
+      [key]: scopeState.model
+    })
+    return {
+      model: nextModel,
+      effect: scopeState.effect
+    }
+  }
+}
+
 
 function handleActions (actionHandlers) {
   return reduceUpdates(
@@ -35,7 +80,7 @@ function handleActions (actionHandlers) {
 }
 
 function reduceUpdates (updates) {
-  return function (model, action) {
+  return function reducedUpdate (model, action) {
     return updates.reduce(
       (state, update) => {
         const nextState = update(state.model, action)
@@ -57,7 +102,8 @@ function reduceUpdates (updates) {
 function runMany (runs) {
   return function runEffect (effect, sources) {
     const nextActions = runs.map(run => {
-      return run(effect, sources) || pull.empty()
+      if (run == null) return empty()
+      return run(effect, sources) || empty()
     })
 
     return pullMany(nextActions)
@@ -65,3 +111,13 @@ function runMany (runs) {
 }
 
 function identity (id) { return id }
+
+/*
+function mapValues (object, lambda) {
+  return Object.keys(object).reduce(sofar, key => {
+    return assign(sofar, {
+      [key]: lambda(object[key], key, object)
+    })
+  }, {})
+}
+*/
