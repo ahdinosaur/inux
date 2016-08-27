320a0b2330ad0f3ab0ece01be9f15fc70c6e6a4c
Michael Williams <michael.williams@enspiral.com>
2016-08-27 16:59:26 +1200

    upgrade sheet-router to 3.2.0

--- router.js
+++ router.js
@@ -4,11 +4,5 @@ module.exports = Router
 
 function Router (routes) {
   const [lastPath] = routes[routes.length - 1]
-  return createRouter(lastPath, function createRoutes (createRoute) {
-    return routes.map(route => {
-      const [path, handler, children] = route
-      const childRoutes = children ? createRoutes(children) : null
-      return createRoute(path, handler, childRoutes)
-    })
-  })
+  return createRouter(lastPath, routes)
 }
